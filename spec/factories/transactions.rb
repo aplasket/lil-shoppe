37f73d703dee049ba3d567383d3368163bb68d54
Alex Kiselich <alexander.kiselich@gmail.com>
2023-06-01 11:12:56 -0600

    Updated spec tests for Invoices

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :transaction do
     credit_card_number { "MyString" }
-    credit_card_expiration { "2023-05-31" }
+    credit_card_expiration_date { "2023-05-31" }
     result { false }
     invoice { nil }
   end
