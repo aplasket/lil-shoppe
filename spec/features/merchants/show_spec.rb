6983e0a14342251171442e18426d77ac17bd7b94
Jeff Nelson <jpnelson85@gmail.com>
2023-06-02 16:04:55 -0500

    start user story 3

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -11,5 +11,34 @@ RSpec.describe Merchant, type: :feature do
       expect(page).to have_content(merchant_1.name)
       expect(page).to_not have_content(merchant_2.name)
     end
+
+
+
+
+
+
+    it "display names of top 5 customers with largest successful transactions " do
+      visit "/merchants/#{merchant_1.id}/dashboard"
+
+
+    end
+
+    it "displays number of successful transactions for top 5 customers" do
+      visit "/merchants/#{merchant_1.id}/dashboard"
+
+      
+    end
+
+
+
+
   end
 end
+
+
+
+
+
+
+
+
