e982c9b79d3cdcda694966f899b6a9864bde6e04
Ashley Plasket <ashley.plasket@gmail.com>
2023-06-02 17:05:18 -0400

    combine all factory files into one file

--- spec/features/admin/index.spec.rb
+++ spec/features/admin/index.spec.rb
@@ -17,4 +17,44 @@ RSpec.describe "/admin, index page", type: :feature do
       end
     end
   end
+
+  describe "Admin Dashboard Stats - Top Customers" do
+    let!(:customer1) { create(:customer) } # 5 successful transactions
+    let!(:customer2) { create(:customer) } # 4 successful transactions
+    let!(:customer3) { create(:customer) } # 3 successful transactions
+    let!(:customer4) { create(:customer) } # 2 successful transactions
+    let!(:customer5) { create(:customer) } # 1 successful transactions
+    let!(:customer6) { create(:customer) } # 0 successful transactions
+
+    before(:each) do
+      5.times do
+        invoice = create(:invoice, customer: customer1)
+        create(:transaction, result: true, invoice: invoice)
+      end
+
+      4.times do
+        invoice = create(:invoice, customer: customer2)
+        create(:transaction, result: true, invoice: invoice)
+      end
+
+      3.times do
+        invoice = create(:invoice, customer: customer3)
+        create(:transaction, result: true, invoice: invoice)
+      end
+
+      2.times do
+        invoice = create(:invoice, customer: customer4)
+        create(:transaction, result: true, invoice: invoice)
+      end
+
+      invoice = create(:invoice, customer: customer5)
+      create(:transaction, result: true, invoice: invoice)
+      create(:transaction, result: false, invoice: invoice)
+
+      2.times do
+        invoice = create(:invoice, customer: customer6)
+        create(:transaction, result: false, invoice: invoice)
+      end
+    end
+  end
 end
