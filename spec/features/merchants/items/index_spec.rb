c72fa7839acc5750b0b46851a7676fdeade2992a
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-06-02 16:34:47 -0600

    Update verbiage for consistency

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "Merchants items index" do
+RSpec.describe "Merchant_items#index" do
   let!(:merchant_1) { Merchant.create!(name: "Steve's Soaps") }
   let!(:merchant_2) { Merchant.create!(name: "Charlie's Chia Pets") }
   let!(:item_1) { merchant_1.items.create!(name: "hand soap", description: "lavender", unit_price: 800) }
