411f49ec9819b3a81e5e7855cfefbd2fff7cd594
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-06-02 17:45:29 -0600

    Refactor currency conversion and file placement in directory

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,4 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
-
-  def price_in_dollars
-    sprintf("$%.2f", unit_price / 100.to_f)
-  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,14 +4,4 @@ RSpec.describe Item, type: :model do
   it { should belong_to :merchant }
   it { should have_many :invoice_items }
   it { should have_many(:invoices).through(:invoice_items) }
-
-  let!(:merchant_1) { Merchant.create!(name: "Steve's Soaps") }
-  let!(:merchant_2) { Merchant.create!(name: "Charlie's Chia Pets") }
-  let!(:item_1) { merchant_1.items.create!(name: "hand soap", description: "lavender", unit_price: 800) }
-  let!(:item_2) { merchant_1.items.create!(name: "sugar scrub", description: "lemongrass", unit_price: 1000) }
-  let!(:item_3) { merchant_2.items.create!(name: "Bob Ross Chia", description: "medium chia pet", unit_price: 1500) }
-
-  it "returns the price in dollars" do
-    expect(item_1.price_in_dollars).to eq("$8.00")
-  end
 end
