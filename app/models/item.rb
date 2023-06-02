7de2ad3c3f2f67ff8f93c52f4763d29083f80c14
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-06-02 17:28:33 -0600

    Feat: Add #show for merchant_items, conversion to $ for items, test for model/item

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+
+  def price_in_dollars
+    sprintf("$%.2f", unit_price / 100.to_f)
+  end
 end
--- spec/features/merchants/items/show_spec.rb
+++ spec/features/merchants/items/show_spec.rb
@@ -1,30 +1,25 @@
 require "rails_helper"
 
 RSpec.describe "Merchant_items#show" do
-# 7. Merchant Items Show Page
+  let!(:merchant_1) { Merchant.create!(name: "Steve's Soaps") }
+  let!(:merchant_2) { Merchant.create!(name: "Charlie's Chia Pets") }
+  let!(:item_1) { merchant_1.items.create!(name: "hand soap", description: "lavender", unit_price: 800) }
+  let!(:item_2) { merchant_1.items.create!(name: "sugar scrub", description: "lemongrass", unit_price: 1000) }
+  let!(:item_3) { merchant_2.items.create!(name: "Bob Ross Chia", description: "medium chia pet", unit_price: 1500) }
 
-# As a merchant,
-# When I click on the name of an item from the merchant items index page, (merchants/:merchant_id/items)
-# Then I am taken to that merchant's item's show page (/merchants/:merchant_id/items/:item_id)
-# And I see all of the item's attributes including:
+  it "links to the merchant's item's show page" do
+    visit "/merchants/#{merchant_1.id}/items"
 
-# - Name
-# - Description
-# - Current Selling Price
-let!(:merchant_1) { Merchant.create!(name: "Steve's Soaps") }
-let!(:merchant_2) { Merchant.create!(name: "Charlie's Chia Pets") }
-let!(:item_1) { merchant_1.items.create!(name: "hand soap", description: "lavender", unit_price: 800) }
-let!(:item_2) { merchant_1.items.create!(name: "sugar scrub", description: "lemongrass", unit_price: 1000) }
-let!(:item_3) { merchant_2.items.create!(name: "Bob Ross Chia", description: "medium chia pet", unit_price: 1500) }
+    click_link "hand soap"
+    save_and_open_page
 
-it "links to the merchant's item's show page"
-  visit "/merchants/#{merchant_1.id}/items"
-
-  click_link "item_1.name"
-
-  expect(current_path).to eq "/merchants/#{merchant_1.id}/items/#{item_1.id}"
-  expect(page).to have_content(item_1.name)
-  expect(page).to have_content(item_1.description)
-  expect(page).to have_content(item_1.unit_price)
+    expect(current_path).to eq "/merchants/#{merchant_1.id}/items/#{item_1.id}"
+    expect(page).to have_content("Name")
+    expect(page).to have_content(item_1.name)
+    expect(page).to have_content("Description")
+    expect(page).to have_content(item_1.description)
+    expect(page).to have_content("Current Selling Price")
+    expect(page).to have_content("$8.00")
+  end
 end
 
