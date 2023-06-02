0a2a5f0ba8547841a5e0986dee3fcf9a043bdc84
Jeff Nelson <jpnelson85@gmail.com>
2023-06-01 19:20:59 -0500

    finish user story 1

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
 
   get "/", to: "application#welcome"
 
-  get "/merchants/:id/dashboard", to: "merchants#show"
+  get "/merchants/:merchant_id/dashboard", to: "merchants#show"
 end
--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -1,12 +1,6 @@
 require "rails_helper"
 
 RSpec.describe Merchant, type: :feature do
-    # 1. Merchant Dashboard
-
-    # As a merchant,
-    # When I visit my merchant dashboard (/merchants/:merchant_id/dashboard)
-    # Then I see the name of my merchant
-
   describe "as a merchant, I visit my dashboard" do
     let!(:merchant_1) { Merchant.create!(name: "Steve's Soaps") }
     let!(:merchant_2) { Merchant.create!(name: "Charlie's Chia Pets") }
@@ -14,11 +8,8 @@ RSpec.describe Merchant, type: :feature do
     it "has the name of my merchant" do
       visit "/merchants/#{merchant_1.id}/dashboard"
 
-      save_and_open_page
-
-      within("#merchant-#{merchant_1.id}") do
-        expect(page).to have_content(merchant_1.name)
-      end
+      expect(page).to have_content(merchant_1.name)
+      expect(page).to_not have_content(merchant_2.name)
     end
   end
 end
