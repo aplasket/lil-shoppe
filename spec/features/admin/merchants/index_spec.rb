6600715da79bfb557b3577e055fd973d7248691d
Ashley Plasket <ashley.plasket@gmail.com>
2023-06-03 17:20:40 -0400

    add show action to admin::merchants controller, tests, and view

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -7,22 +7,9 @@ RSpec.describe "/admin/merchants, admin::merchants index page" do
   describe "as an admin on the admin::merchants index page" do
     it "I see the name of each merchant and their links" do
       visit "/admin/merchants"
-      save_and_open_page
+
       expect(page).to have_link(merchant_1.name)
       expect(page).to have_link(merchant_2.name)
-    end
-
-    it "each merchant link navigates to admin::merchants show page" do
-      visit "/admin/merchants"
-      click_link("#{merchant_1.name}")
-
-      expect(current_path).to eq("admin/merchants/#{merchant_1.id}")
-      expect(page).to have_content(merchant_1.name)
-      expect(page).to_not have_content(merchant_2.name)
-
-      visit "/admin/merchants/#{merchant_2.id}"
-      expect(page).to have_content(merchant_2.name)
-      expect(page).to_not have_content(merchant_1.name)
     end
   end
 end
