8e598991a2d317dfd0fb063ec5d58938d4697a43
Alex Kiselich <alexander.kiselich@gmail.com>
2023-06-03 13:05:48 -0600

    Updated invoice model test spec to show sort ability

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -9,8 +9,8 @@ RSpec.describe Invoice, type: :model do
     it { should have_many(:merchants).through(:items) }
   end
   describe "class methods" do
-    describe "#find_incomplete_invoices" do
-      it "returns all the invoices that have items that have not been shipped yet" do
+    describe "#find_and_sort_incomplete_invoices" do
+      it "returns all the invoices that have items that have not been shipped yet, I see the creation date ordered by oldest to newest" do
         merchant = create(:merchant)
         customer = create(:customer)
 
@@ -21,9 +21,9 @@ RSpec.describe Invoice, type: :model do
         item_5 = create(:item, merchant: merchant)
         item_6 = create(:item, merchant: merchant)
 
-        invoice_1 = create(:invoice, customer: customer) # 2 items that have not shipped
-        invoice_2 = create(:invoice, customer: customer) # 1 item shipped, 1 not shipped
-        invoice_3 = create(:invoice, customer: customer) # 2 items that have shipped
+        invoice_1 = create(:invoice, created_at: 3.day.ago, customer: customer) # 2 items that have not shipped
+        invoice_2 = create(:invoice, created_at: 2.day.ago, customer: customer) # 1 item shipped, 1 not shipped
+        invoice_3 = create(:invoice, created_at: 2.day.ago, customer: customer) # 2 items that have shipped
 
         invoice_item_1 = create(:invoice_item, status: "pending", item: item_1, invoice: invoice_1)
         invoice_item_2 = create(:invoice_item, status: "packaged", item: item_2, invoice: invoice_1)
@@ -31,8 +31,8 @@ RSpec.describe Invoice, type: :model do
         invoice_item_4 = create(:invoice_item, status: "shipped", item: item_4, invoice: invoice_2)
         invoice_item_5 = create(:invoice_item, status: "shipped", item: item_5, invoice: invoice_3)
         invoice_item_6 = create(:invoice_item, status: "shipped", item: item_6, invoice: invoice_3)
-        
-        expect(Invoice.find_incomplete_invoices).to eq([invoice_2, invoice_1])
+
+        expect(Invoice.find_and_sort_incomplete_invoices).to eq([invoice_1, invoice_2])
       end
     end
 
