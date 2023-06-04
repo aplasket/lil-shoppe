f383dc2c53f76030c870244320bdb7294914a235
Myles Nottingham <MylesNottingham@gmail.com>
2023-06-03 19:13:09 -0600

    Refactor: Shorten lines, change quotes, & add line

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -13,21 +13,25 @@ RSpec.describe "Admin Invoices Show Page", type: :feature do
 
       expect(page).to have_content("Invoice ID: #{invoice_1.id}")
       expect(page).to have_content("Invoice Status: #{invoice_1.status}")
-      expect(page).to have_content("Invoice Created On: #{invoice_1.created_at.strftime("%A, %B %d, %Y")}")
+      expect(page).to have_content("Invoice Created On: #{invoice_1.created_at.strftime('%A, %B %d, %Y')}")
       expect(page).to have_content("Customer Name: #{customer_1.first_name} #{customer_1.last_name}")
 
       expect(page).to_not have_content("Invoice ID: #{invoice_2.id}")
-      expect(page).to_not have_content("Customer Name: #{invoice_2.customer.first_name} #{invoice_2.customer.last_name}")
+      expect(page).to_not have_content(
+        "Customer Name: #{invoice_2.customer.first_name} #{invoice_2.customer.last_name}"
+      )
 
       visit admin_invoice_path(invoice_2)
 
       expect(page).to have_content("Invoice ID: #{invoice_2.id}")
       expect(page).to have_content("Invoice Status: #{invoice_2.status}")
-      expect(page).to have_content("Invoice Created On: #{invoice_2.created_at.strftime("%A, %B %d, %Y")}")
+      expect(page).to have_content("Invoice Created On: #{invoice_2.created_at.strftime('%A, %B %d, %Y')}")
       expect(page).to have_content("Customer Name: #{customer_2.first_name} #{customer_2.last_name}")
 
       expect(page).to_not have_content("Invoice ID: #{invoice_1.id}")
-      expect(page).to_not have_content("Customer Name: #{invoice_1.customer.first_name} #{invoice_1.customer.last_name}")
+      expect(page).to_not have_content(
+        "Customer Name: #{invoice_1.customer.first_name} #{invoice_1.customer.last_name}"
+      )
     end
   end
 end
