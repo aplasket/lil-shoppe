ee9460de3e5906e0be96b37ca820180d7335c210
Ashley Plasket <ashley.plasket@gmail.com>
2023-06-04 16:25:07 -0400

    remove update feature, move to admin/merch

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -3,19 +3,4 @@ class MerchantsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @customers = Customer.top_5_by_transaction
   end
-
-  def update
-    merchant = Merchant.find(params[:id])
-    if params[:merchant][:name]
-      if merchant.update(merchant_params)
-        redirect_to admin_merchant_path(merchant)
-        flash[:success] = "Merchant has been successfully updated"
-      end
-    end
-  end
-
-  private
-  def merchant_params
-    params.require(:merchant).permit(:name)
-  end
 end
