79cb7309878365fbd827b2fd23538e005c134537
Ashley Plasket <ashley.plasket@gmail.com>
2023-06-04 18:40:11 -0400

    add model method for enable/disable status

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,5 +1,13 @@
 class Merchant < ApplicationRecord
   has_many :items
-  
+
   enum status: [:enabled, :disabled]
+
+  def self.all_enabled
+    where(status: 0)
+  end
+
+  def self.all_disabled
+    where(status: 1)
+  end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,5 +1,21 @@
 require "rails_helper"
 
 RSpec.describe Merchant, type: :model do
-  it { should have_many :items }
+  describe "relationships" do
+    it { should have_many :items }
+  end
+
+  describe "class methods" do
+    let!(:merchant_1) { create(:merchant) }
+    let!(:merchant_2) { create(:merchant, status: 1) }
+    let!(:merchant_3) { create(:merchant, status: 1) }
+
+    it "#all_enabled" do
+      expect(Merchant.all_enabled).to eq([merchant_1])
+    end
+
+    it "#all_disabled" do
+      expect(Merchant.all_disabled).to eq([merchant_2, merchant_3])
+    end
+  end
 end
