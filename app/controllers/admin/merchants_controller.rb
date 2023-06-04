ee6d6e170f9c79a1ffef7e68844bf7071bd4baf8
Ashley Plasket <ashley.plasket@gmail.com>
2023-06-04 16:25:31 -0400

    add update feature to adminmerch controller

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -10,4 +10,19 @@ class Admin::MerchantsController < ApplicationController
   def edit
     @merchant = Merchant.find(params[:id])
   end
+
+  def update
+    merchant = Merchant.find(params[:id])
+    if params[:merchant][:name]
+      if merchant.update(merchant_params)
+        redirect_to admin_merchant_path(merchant)
+        flash[:success] = "Merchant has been successfully updated"
+      end
+    end
+  end
+
+  private
+  def merchant_params
+    params.require(:merchant).permit(:name)
+  end
 end
