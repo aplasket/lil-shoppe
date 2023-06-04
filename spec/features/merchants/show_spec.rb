699cfa8275d9a78ddd09170cc44c88611bde2dac
Jeff Nelson <jpnelson85@gmail.com>
2023-06-04 10:08:45 -0500

    create tests for show_spec

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -6,28 +6,28 @@ RSpec.describe Merchant, type: :feature do
     let!(:merchant_2) { Merchant.create!(name: "Charlie's Chia Pets") }
 
     it "has the name of my merchant" do
-      visit merchant_dashboard_path(merchant_1)
+      visit "/merchants/#{merchant_1.id}/dashboard"
 
       expect(page).to have_content(merchant_1.name)
       expect(page).to_not have_content(merchant_2.name)
     end
 
     it "displays link to merchant items index" do
-      visit merchant_dashboard_path(merchant_1)
+      visit "/merchants/#{merchant_1.id}/dashboard"
 
       click_link "Items"
 
-      expect(current_path).to eq(merchant_items_path(merchant_1))
-      expect(current_path).to_not eq(merchant_items_path(merchant_2))
+      expect(current_path).to eq("/merchants/#{merchant_1.id}/items")
+      expect(current_path).to_not eq("/merchants/#{merchant_2.id}/items")
     end
 
     it "displays link to merchant invoices index" do
-      visit merchant_dashboard_path(merchant_1)
+      visit "/merchants/#{merchant_1.id}/dashboard"
 
       click_link "Invoices"
 
-      expect(current_path).to eq(merchant_invoices_path(merchant_1))
-      expect(current_path).to_not eq(merchant_invoices_path(merchant_2))
+      expect(current_path).to eq("/merchants/#{merchant_1.id}/invoices")
+      expect(current_path).to_not eq("/merchants/#{merchant_2.id}/invoices")
     end
   end
 
@@ -40,7 +40,7 @@ RSpec.describe Merchant, type: :feature do
     let!(:customer_4) { create(:customer) } # 2 successful transactions
     let!(:customer_5) { create(:customer) } # 1 successful transactions
     let!(:customer_6) { create(:customer) } # 0 successful transactions
-
+    
     before(:each) do
       5.times do
         invoice = create(:invoice, customer: customer_1)
@@ -71,9 +71,10 @@ RSpec.describe Merchant, type: :feature do
         create(:transaction, result: false, invoice: invoice)
       end
     end
-
+    
     it "display names of top 5 customers with largest successful transactions " do
-      visit merchant_dashboard_path(merchant_1)
+    
+      visit "/merchants/#{merchant_1.id}/dashboard"
 
       within("#top5") do
         expect(customer_1.first_name).to appear_before(customer_2.first_name)
@@ -85,31 +86,69 @@ RSpec.describe Merchant, type: :feature do
     end
 
     it "displays number of successful transactions for top 5 customers" do
-      visit merchant_dashboard_path(merchant_1)
+      visit "/merchants/#{merchant_1.id}/dashboard"
 
       within("#top5") do
-        expect(
-          "#{customer_1.successful_transactions_count} successful transactions"
-        ).to appear_before(
-          "#{customer_2.successful_transactions_count} successful transactions"
-        )
-        expect(
-          "#{customer_2.successful_transactions_count} successful transactions"
-        ).to appear_before(
-          "#{customer_3.successful_transactions_count} successful transactions"
-        )
-        expect(
-          "#{customer_3.successful_transactions_count} successful transactions"
-        ).to appear_before(
-          "#{customer_4.successful_transactions_count} successful transactions"
-        )
-        expect(
-          "#{customer_4.successful_transactions_count} successful transactions"
-        ).to appear_before(
-          "#{customer_5.successful_transactions_count} successful transactions"
-        )
-        expect(page).to_not have_content(customer_6.successful_transactions_count.to_s)
+        expect("#{customer_1.successful_transactions_count} successful transactions").to appear_before("#{customer_2.successful_transactions_count} successful transactions")
+        expect("#{customer_2.successful_transactions_count} successful transactions").to appear_before("#{customer_3.successful_transactions_count} successful transactions")
+        expect("#{customer_3.successful_transactions_count} successful transactions").to appear_before("#{customer_4.successful_transactions_count} successful transactions")
+        expect("#{customer_4.successful_transactions_count} successful transactions").to appear_before("#{customer_5.successful_transactions_count} successful transactions")
+        expect(page).to_not have_content("#{customer_6.successful_transactions_count}")
       end
+    end
+  end
+
+  describe "Merchant Dashboard Stats - Merchant Dashboard Items Ready to Ship" do
+    let!(:merchant_1) { create(:merchant) }
+    let!(:merchant_2) { create(:merchant) }
+    let!(:customer_1) { create(:customer) } 
+    let!(:customer_2) { create(:customer) } 
+    let!(:customer_3) { create(:customer) }
+    let!(:item_1) { create(:item, merchant: merchant_1) }
+    let!(:item_2) { create(:item, merchant: merchant_1) }
+    let!(:item_3) { create(:item, merchant: merchant_1) }
+    let!(:item_4) { create(:item, merchant: merchant_2) }
+    let!(:invoice_items_1) { create(:invoice_item, item: item_1) }
+    let!(:invoice_items_1) { create(:invoice_item, item: item_2) }
+    let!(:invoice_items_2) { create(:invoice_item, item: item_3) }
+    let!(:invoice_items_3) { create(:invoice_item, item: item_4) }
+    let!(:invoice_1) { create(:invoice, customer: customer_1) }
+    let!(:invoice_2) { create(:invoice, customer: customer_2) }
+    let!(:invoice_3) { create(:invoice, customer: customer_3) }
+    
+    before(:each) do
+      5.times do
+        invoice_items = create(:invoice_item, item: item_1)
+        invoice = create(:invoice, customer: customer_1)
+        create(:transaction, result: true, invoice: invoice)
+      end
+
+      4.times do
+        invoice = create(:invoice, customer: customer_2)
+        create(:transaction, result: false, invoice: invoice)
+      end
+
+      3.times do
+        invoice = create(:invoice, customer: customer_3)
+        create(:transaction, result: false, invoice: invoice)
+      end
+
+    end
+    it 'display a section with "Items Ready to Ship" with a list of all those items' do
+      visit "/merchants/#{merchant_1.id}/dashboard"
+
+      within("#ItemsReadyShip") do
+        expect(page).to have_content("Items Ready to Ship")
+
+
+      end
+    end
+
+    it 'display id of the invoice that ordered the item as a link to that merchant show page' do
+      visit "/merchants/#{merchant_1.id}/dashboard"
+
+      within("#ItemsReadyShip") do
+
     end
   end
 end
