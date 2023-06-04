cc7895da7c87f4de4c6cd3ff2349647c55bbff6e
Myles Nottingham <MylesNottingham@gmail.com>
2023-06-03 18:01:27 -0600

    Refactor: Nest, Slim, & add Path Helpers

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -1,23 +1,20 @@
-class MerchantItemsController < ApplicationController
+class Merchant::ItemsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
   end
 
   def show
     @merchant = Merchant.find(params[:merchant_id])
-    @item = Item.find(params[:item_id])
   end
 
   def edit
     @merchant = Merchant.find(params[:merchant_id])
-    @item = Item.find(params[:item_id])
   end
 
   def update
-    @merchant = Merchant.find(params[:merchant_id])
-    @item = Item.find(params[:item_id])
-    if @item.update(item_params)
-      redirect_to "/merchants/#{@merchant.id}/items/#{@item.id}"
+    merchant = Merchant.find(params[:merchant_id])
+    if merchant.items.find(params[:item_id]).update(item_params)
+      redirect_to merchant_item_path(merchant, merchant.items.find(params[:item_id]))
       flash[:success] = "** Item information has been successfully updated **"
     end
   end
