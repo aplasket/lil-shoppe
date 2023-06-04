053fc554fd64f898d4802daf820b6d4510f1b081
Ashley Plasket <ashley.plasket@gmail.com>
2023-06-03 21:30:05 -0400

    add edit functionality to controller, view, and tests

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -10,17 +10,4 @@ class Admin::MerchantsController < ApplicationController
   def edit
     @merchant = Merchant.find(params[:id])
   end
-
-  def update
-    @merchant = Merchant.find(params[:id])
-    if @merchant.update(merchant_params)
-      redirect_to "/admin/merchants/#{@merchant.id}"
-      flash[:success] = "Merchant has been successfully updated"
-    end
-  end
-
-  private
-  def merchant_params
-    params.permit(:name)
-  end
 end
--- spec/features/admin/merchants/edit_spec.rb
+++ spec/features/admin/merchants/edit_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "/admin/merchants/:id/edit, admin::merchants edit page" do
       merchant_2 = Merchant.create!(name: "CandyMan")
       merchant_3 = Merchant.create!(name: "Toasty Toads")
 
-      visit "/admin/merchants/#{merchant_3.id}"
+      visit admin_merchant_path(merchant_3)
 
       expect(page).to have_content(merchant_3.name)
       expect(page).to have_link("Update #{merchant_3.name}")
@@ -19,7 +19,7 @@ RSpec.describe "/admin/merchants/:id/edit, admin::merchants edit page" do
       fill_in "Name", with: "Cheesy Bread"
       click_button "Submit Updates"
 
-      expect(current_path).to eq("/admin/merchants/#{merchant_3.id}")
+      expect(current_path).to eq(admin_merchant_path(merchant_3))
       expect(page).to have_content("Merchant has been successfully updated")
       expect(page).to have_content("Cheesy Bread")
       expect(page).to_not have_content("Toasty Toads")
