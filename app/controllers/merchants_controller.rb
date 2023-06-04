f5614fad757e2f3be598f3b4ba1c8e6ba50d6098
Ashley Plasket <ashley.plasket@gmail.com>
2023-06-03 21:29:02 -0400

    add update function to merchants controller

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -3,4 +3,22 @@ class MerchantsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @customers = Customer.top_5_by_transaction
   end
+
+  def update
+    merchant = Merchant.find(params[:id])
+    if params[:merchant][:name]
+      if merchant.update(merchant_params)
+        redirect_to admin_merchant_path(merchant)
+        flash[:success] = "Merchant has been successfully updated"
+      else
+        redirect_to admin_merchant_path(merchant)
+        flash[:alert] = "Error: #{merchant.errors.full_messages.to_sentence}"
+      end
+    end
+  end
+
+  private
+  def merchant_params
+    params.require(:merchant).permit(:name)
+  end
 end
