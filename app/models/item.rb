d05f00a90a2c9b40983f472aee3e5168daf9f7d5
Alex Kiselich <alexander.kiselich@gmail.com>
2023-06-05 12:54:52 -0600

    Created quantity sold, find sold price and invoice item status methods

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,19 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+  has_many :transactions, through: :invoices
+  has_many :customers, through: :invoices
+
+  def quantity_sold(invoice)
+    invoice_items.where(invoice_id: invoice.id).pluck(:quantity)
+  end
+
+  def find_sold_price(invoice)
+    invoice_items.where(invoice_id: invoice.id).pluck(:unit_price)
+  end
+
+  def invoice_item_status(invoice)
+    invoice_items.where(invoice_id: invoice.id).pluck(:status)
+  end
 end
+
