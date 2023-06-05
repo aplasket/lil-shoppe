596d18c2dbd5e69c4410019171c69b9e6ee5353a
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-06-05 15:40:53 -0600

    Add full test coverage for #merchant/items/index

--- spec/features/merchant/items/index_spec.rb
+++ spec/features/merchant/items/index_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe "Merchant_items#index", type: :feature do
   let!(:merchant_2) { Merchant.create!(name: "Charlie's Chia Pets") }
   let!(:item_1) { merchant_1.items.create!(name: "hand soap", description: "lavender", unit_price: 800, status: "enabled") }
   let!(:item_2) { merchant_1.items.create!(name: "sugar scrub", description: "lemongrass", unit_price: 1000, status: "enabled") }
-  let!(:item_3) { merchant_2.items.create!(name: "Bob Ross Chia", description: "medium chia pet", unit_price: 1500, status: "enabled") }
+  let!(:item_3) { merchant_2.items.create!(name: "Bob Ross Chia", description: "medium chia pet", unit_price: 1500, status: "disabled") }
 
   it "shows all of the names of all of the merchant's items" do
     visit merchant_items_path(merchant_id: merchant_1.id)
 
     expect(page).to have_content(item_1.name)
     expect(page).to have_content(item_2.name)
-    expect(page).to have_no_content(item_3.name)
+    expect(page).to_not have_content(item_3.name)
   end
 
   it "displays a button that will enable or disable each item" do
@@ -37,11 +37,13 @@ RSpec.describe "Merchant_items#index", type: :feature do
       expect(page).to_not have_content(item_2.name)
 
       expect(page).to have_button("Enable #{item_1.name}")
+      expect(page).to_not have_button("Disable #{item_2.name}")
+
+      click_button "Enable #{item_1.name}"
     end
 
     within ".enabled-items" do
-      save_and_open_page
-      expect(page).to_not have_link(item_1.name)
+      expect(page).to_not have_link(item_3.name)
       expect(page).to_not have_button("Enable #{item_2.name}")
     end
   end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,4 +4,25 @@ RSpec.describe Item, type: :model do
   it { should belong_to :merchant }
   it { should have_many :invoice_items }
   it { should have_many(:invoices).through(:invoice_items) }
+
+  let!(:merchant_1) { create(:merchant) }
+  let!(:merchant_2) { create(:merchant) }
+
+  let!(:item_1) { create(:item, merchant_id: merchant_1.id, status: 0)}
+  let!(:item_2) { create(:item, merchant_id: merchant_1.id, status: 1)}
+  let!(:item_3) { create(:item, merchant_id: merchant_1.id, status: 1)}
+
+  describe "class methods" do
+    describe "#sort_enabled" do
+      it "sorts the items by the enabled status" do
+        expect(merchant_1.items.sort_enabled).to eq([item_2, item_3])
+      end
+    end
+
+    describe "#sort_disabled" do
+      it "sorts the items by disabled status" do
+        expect(merchant_1.items.sort_disabled).to eq([item_1])
+      end
+    end
+  end
 end
