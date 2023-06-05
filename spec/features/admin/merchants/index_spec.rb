b11a090aafc63bc3d1d551d745673314e58b274c
Ashley Plasket <ashley.plasket@gmail.com>
2023-06-04 21:21:01 -0400

    fix test for update status of given merchant

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 RSpec.describe "/admin/merchants, admin::merchants index page" do
   let!(:merchant_1) { create(:merchant) }
   let!(:merchant_2) { create(:merchant, status: 1) }
+  let!(:merchant_3) { create(:merchant, status: 1) }
 
   describe "as an admin on the admin::merchants index page" do
     it "I see the name of each merchant and their links - US24" do
@@ -34,29 +35,33 @@ RSpec.describe "/admin/merchants, admin::merchants index page" do
     it "updates status of given merchant when clicked - US27" do
       visit admin_merchants_path
 
+      expect(merchant_1.status).to eq("enabled")
+      expect(merchant_2.status).to eq("disabled")
+      expect(merchant_3.status).to eq("disabled")
+
       within ".merchant-#{merchant_1.id}" do
-        expect(merchant_1.status).to eq("enabled")
         expect(page).to have_button("Disable")
         expect(page).to_not have_button("Enable")
 
         click_button "Disable"
 
         expect(current_path).to eq(admin_merchants_path)
-        expect(merchant_1.status).to eq("disabled")
         expect(page).to have_button("Enable")
       end
 
       within ".merchant-#{merchant_2.id}" do
-        expect(merchant_1.status).to eq("disabled")
         expect(page).to have_button("Enable")
         expect(page).to_not have_button("Disable")
 
         click_button "Enable"
 
         expect(current_path).to eq(admin_merchants_path)
-        expect(merchant_2.status).to eq("enabled")
         expect(page).to have_button("Disable")
       end
+
+      expect(merchant_1.reload.status).to eq("disabled")
+      expect(merchant_2.reload.status).to eq("enabled")
+      expect(merchant_3.reload.status).to eq("disabled")
     end
   end
 end
