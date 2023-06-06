147a7c9bb5564c272161abed2433b5b8596c0175
Jeff Nelson <jpnelson85@gmail.com>
2023-06-06 15:54:31 -0500

    update merchant model test

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,17 +4,16 @@ RSpec.describe Merchant, type: :model do
   it { should have_many :items }
 
   it "returns items that are ready to be shipped" do
-    item1 = create(:item)
-    item2 = create(:item)
+    merchant = create(:merchant)
+    item1 = create(:item, merchant: merchant)
+    item2 = create(:item, merchant: merchant)
     invoice1 = create(:invoice, status: 0)
     invoice2 = create(:invoice, status: 1)
     invoice3 = create(:invoice, status: 2)
     create(:invoice_item, item: item1, invoice: invoice1)
     create(:invoice_item, item: item2, invoice: invoice2)
         
-    items_ready_to_ship = Item.joins(:invoices)
-      .where("invoices.status = ?", 0)
-      .select("items.*")
+    items_ready_to_ship = Item.joins(:invoices).where("invoices.status = ?", 0).select("items.*")
     
     expect(items_ready_to_ship).to include(item1)
     expect(items_ready_to_ship).not_to include(item2)
