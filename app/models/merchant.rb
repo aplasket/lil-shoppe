78272dfc07f3d97a93b743e8fa4b24a715a270e6
Ashley Plasket <ashley.plasket@gmail.com>
2023-06-05 20:54:57 -0400

    add validations for merchant name

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,5 +1,6 @@
 class Merchant < ApplicationRecord
   has_many :items
+  validates_presence_of :name
 
   enum status: [:enabled, :disabled]
 
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe Merchant, type: :model do
     it { should have_many :items }
   end
 
+  describe "validations" do
+    it { should validate_presence_of :name }
+  end
+
   describe "class methods" do
     let!(:merchant_1) { create(:merchant, status: 0) }
     let!(:merchant_2) { create(:merchant) }
